7b201113e37225acd12a93de49ff1e3cfdedda07
dieyna9 <dieynagoudiaby0911@gmail.com>
2026-09-19 20:58:21 +0000

    Mise à jour du site TBS

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+// T.B.S. — Configuration Supabase
+// Ne mets JAMAIS une clé "Secret" ou "service_role" dans ce fichier.
+
+const SUPABASE_URL = "https://hclmncksgqvlqaenmjja.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_lYQedli3kbo5YzkuuQcWMA_8lc9gLqK";
